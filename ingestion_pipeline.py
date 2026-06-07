af6845587ef47c68be552cd8f703ee61b197d9c0
ghshamad312 <ghshamad312@gmail.com>
2026-06-07 22:53:10 +0500

    completed the data ingestionpipeline

--- ingestion_pipeline.py
+++ ingestion_pipeline.py
@@ -1,6 +1,7 @@
 import os
 from langchain_community.document_loaders import TextLoader, DirectoryLoader
-from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
 from dotenv import load_dotenv
@@ -20,12 +21,44 @@ def doc_loader(docpath):
         print(f"document length = {len(doc.page_content)}")
     return document
 
-    
+def chunker(documents,chunksize, chunkoverlap):
+    splitter=RecursiveCharacterTextSplitter(
+        chunk_size=chunksize,
+        chunk_overlap=chunkoverlap
+    )
+    chunks=splitter.split_documents(documents)
+    print(len(chunks))
+    return chunks    
+
+def vector_store(chunks, presist_loaction):
+    embeddings = HuggingFaceEmbeddings(
+    model_name="all-MiniLM-L6-v2"
+)   
+    store=Chroma.from_documents(
+        documents=chunks,
+        embedding=embeddings,
+        persist_directory=presist_loaction,
+        collection_metadata={"hnsw:space":"cosine"}
+        
+    )
+    print("\n--- CHROMA DATA PREVIEW ---")
+
+    data = store.get(include=["documents", "metadatas"])
+    for i in range(min(5, len(data["documents"]))):
+        print("\nDOC:", data["documents"][i])
+        print("META:", data["metadatas"][i])
+
+    print("\nTotal chunks:", store._collection.count())
+    return store
 
 
 def main():
     print("this is main")
-    doc_loader("./documents/")
+    documents=doc_loader("./documents/")
+    chunks =chunker(documents, 1000, 10)
+    store= vector_store(chunks, "db/chromadb")
+
+
 
 
 
